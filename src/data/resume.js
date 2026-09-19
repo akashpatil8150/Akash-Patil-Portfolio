8a5f16b09033c694f6a5fd6a1b35dad40651c692
Akash Patil <138558303+akashpatil8150@users.noreply.github.com>
2026-09-19 12:46:27 +0530

    Update resume download URL to instant GitHub raw blob URL

--- src/data/resume.js
+++ src/data/resume.js
@@ -1,1 +1,1 @@
-export const RESUME_URL = 'https://raw.githubusercontent.com/akashpatil8150/Akash-Patil-Portfolio/main/assets/Akash_Patil_Resume.pdf'
+export const RESUME_URL = 'https://github.com/akashpatil8150/Akash-Patil-Portfolio/blob/main/assets/Akash_Patil_Resume.pdf?raw=true'
